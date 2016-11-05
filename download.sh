12ff75f1b47371f23fe6ee087d1d6db6ff3d2106
Luca Coviello <luca.coviello@gmail.com>
2016-11-05 16:51:24 +0100

    copy dataset to hdfs

--- download.sh
+++ download.sh
@@ -1,5 +1,9 @@
 YEAR=${1-2008}
+HDFS_DATA_FOLDER="/data"
 
 mkdir -p data
 wget -P data "http://stat-computing.org/dataexpo/2009/${YEAR}.csv.bz2"
-bzip2 -dk "data/${YEAR}.csv.bz2"
+bzip2 -dk "./data/${YEAR}.csv.bz2"
+
+hdfs dfs -mkdir -p $HDFS_DATA_FOLDER
+hdfs dfs -put ./data/${YEAR}.csv $HDFS_DATA_FOLDER
