9a131001fea26dd9c99959da1aa3a2a7392acb7e
Luca Coviello <luca.coviello@gmail.com>
2016-12-01 19:53:00 +0100

    added jesus examle

--- bdproj/src/main/scala/lucnik/App.scala
+++ bdproj/src/main/scala/lucnik/App.scala
@@ -28,7 +28,6 @@ object App {
       .enableHiveSupport()
       .getOrCreate()
 
-    // TODO rly???
     import spark.implicits._
 
     Logger.getRootLogger.setLevel(Level.WARN)
@@ -73,7 +72,48 @@ object App {
 
     data = data.select(columnNames.head, columnNames.tail: _*)
 
+    // could not be needed the print
     print(data.printSchema)
     print(data.show(10))
   }
 }
+
+// 
+// // Exercise 3 by Jesús
+//  val spark = SparkSession
+//     .builder()
+//     .appName("")
+//     .getOrCreate()
+//
+// var df = spark.read
+//     .format("com.databricks.spark.csv")
+//     .option("sep", " ")
+//     .option("header", "false")
+//     .load("file:///filepath.csv")
+//     .withColumnRenamed("_c0", "project_name")
+//     .withColumnRenamed("_c1", "page_title")
+//     .withColumnRenamed("_c2", "num_requests")
+//     .withColumnRenamed("_c3", "content_size")
+//     .select(
+//         col("_c0").as("project_name"),
+//         col("_c1").as("page_title"),
+//         col("_c2").as("long").as("num_requests"),
+//         col("_c3").as("long").as("content_size"),
+//     )
+//
+// val project_summary = df
+//     .groupBy("project_name")
+//     .agg(
+//         count("page_title").as("num_pages"),
+//         sum("content_size").as("content_size"),
+//         avg("num_requests").as("mean_requests")
+//     )
+//
+// project_summary.show
+//
+// val most_visited = df.join(
+//     project_summary.select("project_name", "num_requests"),
+//     "project_name"
+// ).filter(col("num_requests") > col(mean_requests))
+//
+// most_visited.show
