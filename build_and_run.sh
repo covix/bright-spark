a499c74a17b43017c5877d4578e6aef10c8394a9
Luca Coviello <luca.coviello@gmail.com>
2016-12-19 17:13:03 +0100

    added pom

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,5 +1,5 @@
 INPUT_FILE=${1:-"data/2008_1000.csv"}
 
-mvn package -f bdproj && \
+mvn package -f bdproj/pom.xml && \
     /opt/spark/bin/spark-submit --class lucnik.App bdproj/target/bdproj-1.0-SNAPSHOT.jar $INPUT_FILE
 
