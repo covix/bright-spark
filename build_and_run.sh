ca95f0c7700e5fa558bf88dc7c083c01fbeb6a77
Luca Coviello <luca.coviello@gmail.com>
2016-12-19 17:22:08 +0100

    updated with master

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,5 +1,6 @@
 INPUT_FILE=${1:-"data/2008_1000.csv"}
+MASTER=${2:-"local"}
 
 mvn package -f bdproj/pom.xml && \
-    /opt/spark/bin/spark-submit --class lucnik.App bdproj/target/bdproj-1.0-SNAPSHOT.jar $INPUT_FILE
+    /opt/spark/bin/spark-submit --master $MASTER --class lucnik.App bdproj/target/bdproj-1.0-SNAPSHOT.jar $INPUT_FILE
 
